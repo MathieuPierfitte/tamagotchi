ef3c8e4b921c3e9dfa4f6cdf71946ac5c3b86de0
MathieuPierfitte <mpierfitte@me.com>
2018-05-31 12:43:05 +1000

    Restrain sleeping possibilities

--- tamagotchi/src/app/services/tamagotchi.controller.ts
+++ tamagotchi/src/app/services/tamagotchi.controller.ts
@@ -48,6 +48,9 @@ export class TamagotchiController {
     get canBeFed(): boolean {
         return this.isAlive && !this.isSleeping && !this.isPlaying;
     }
+    get canSleep(): boolean {
+        return this.isAlive && !this.isPlaying;
+    }
     get canPlay(): boolean {
         return this.isAlive && !this.isSleeping;
     }
